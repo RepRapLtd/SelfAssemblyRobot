f806308517b0dee1469ce1f2b417ded1a23675e2
Adrian Bowyer <adrian@reprappro.com>
2020-09-23 18:25:24 +0100

    Lots of updates...

--- Software/src/GCodeGenerator.cpp
+++ Software/src/GCodeGenerator.cpp
@@ -3,19 +3,37 @@
 // Author      : Adrian Bowyer
 // Version     :
 // Copyright   : GPL
-// Description : Hello World in C++, Ansi-style
+// Description : Simple interactive program to generate G codes for geometric shapes
 //============================================================================
+
+/*
+ * Tell it what shape you want and the dimensions and It'll output the appropriate G Codes to
+ * make the shape.  You can repeat this to produce a whole G Code file.
+ *
+ * The input and output is preceded by ";" characters, so you can simply copy and paste
+ * the entire session into a valid G Code file for execution.
+ */
 
 #include <iostream>
 #include <iomanip>
 #include <math.h>
 using namespace std;
 
-double zDepth = -0.3;
-double safeHeight = 1.0;
-double rapidRate = 1000.0;
-double feedRate = 100.0;
-double plungeRate = 50.0;
+/*
+ * Set these to whatever you want before you start.
+ */
+double zCutDepth = -0.3;
+double safeZHeight = 1.0;
+double rapidInAirFeedRate = 1000.0;
+double xyCutFeedRate = 100.0;
+double zCutPlungeRate = 50.0;
+
+// These are used internally
+
+double currentX = 0;
+double currentY = 0;
+double currentZ = 0;
+bool liftZAtEndOfMove = true;
 
 void Prompt()
 {
@@ -24,25 +42,43 @@ void Prompt()
 	cout << ";  s: - square" << endl;
 	cout << ";  r: - rectangle" << endl;
 	cout << ";  o: - drill centre" << endl;
+	cout << ";  m: - move in air to a position" << endl;
+	cout << ";  l: - cut a straight line to a position" << endl;
+	cout << ";  u: - swap lifting at end of moves or not" << endl;
 	cout << ";  h: - print this list" << endl;
-	cout << ";  q: - quit" << endl<< endl;
+	cout << ";  q: - quit" << endl << endl;
+	cout << "; Z is ";
+	if(!liftZAtEndOfMove)
+		cout << "not ";
+	cout << "being lifted at the end of each activity." << endl << endl;
 }
 
 void rectangle(double x, double y)
 {
-	cout << "G1 X" << x/2.0 << " Y" << y/2.0 << " F" << rapidRate << endl;
-	cout << "G1 Z" << zDepth << " F" << plungeRate << endl;
-	cout << "G1 X" << x/2.0 << " Y" << -y/2.0 << " F" << feedRate << endl;
-	cout << "G1 X" << -x/2.0 << " Y" << -y/2.0 << " F" << feedRate << endl;
-	cout << "G1 X" << -x/2.0 << " Y" << y/2.0 << " F" << feedRate << endl;
-	cout << "G1 X" << x/2.0 << " Y" << y/2.0 << " F" << feedRate << endl;
-	cout << "G1 Z" << safeHeight << " F" << rapidRate << endl;
+	cout << "G1 X" << currentX << " Y" << currentY << " F" << rapidInAirFeedRate << endl;
+	currentZ = zCutDepth;
+	cout << "G1 Z" << currentZ << " F" << zCutPlungeRate << endl;
+	cout << "G1 X" << x << " Y" << currentY << " F" << xyCutFeedRate << endl;
+	cout << "G1 X" << x << " Y" << y << " F" << xyCutFeedRate << endl;
+	cout << "G1 X" << currentX << " Y" << y << " F" << xyCutFeedRate << endl;
+	cout << "G1 X" << currentX << " Y" << currentY << " F" << xyCutFeedRate << endl;
+	if(liftZAtEndOfMove)
+	{
+		currentZ = safeZHeight;
+		cout << "G1 Z" << currentZ << " F" << rapidInAirFeedRate << endl;
+	}
 }
+
+// Cut a circle centred at the current position
+// Return the cutter to the centre in air when done
 
 void circle(double r)
 {
-	cout << "G1 X" << r << " Y0.0" << " F" << rapidRate << endl;
-	cout << "G1 Z" << zDepth << " F" << plungeRate << endl;
+	currentZ = safeZHeight;
+	cout << "G1 Z" << currentZ << " F" << rapidInAirFeedRate << endl;
+	cout << "G1 X" << currentX + r << " Y" << currentY << " F" << rapidInAirFeedRate << endl;
+	currentZ = zCutDepth;
+	cout << "G1 Z" << currentZ << " F" << zCutPlungeRate << endl;
 	int polygon = (int)(0.5 + 60*r/10.0);
 	if(polygon < 20)
 		polygon = 20;
@@ -51,17 +87,56 @@ void circle(double r)
 	for(int p = 0; p < polygon; p++)
 	{
 		a += aInc;
-		cout << "G1 X" << r*cos(a) << " Y" << r*sin(a) << " F" << feedRate << endl;
+		double x = r*cos(a);
+		double y = r*sin(a);
+		cout << "G1 X" << currentX + x << " Y" << currentY + y << " F" << xyCutFeedRate << endl;
 	}
-	cout << "G1 Z" << safeHeight << " F" << rapidRate << endl;
+	currentZ = safeZHeight;
+	cout << "G1 Z" << currentZ << " F" << rapidInAirFeedRate << endl;
+	cout << "G1 X" << currentX << " Y" << currentY << " F" << rapidInAirFeedRate << endl;
 }
 
+// Mark a centre spot.
 
 void centre(double x, double y)
 {
-	cout << "G1 X" << x << " Y" << y << " F" << rapidRate << endl;
-	cout << "G1 Z" << zDepth << " F" << plungeRate << endl;
-	cout << "G1 Z" << safeHeight << " F" << rapidRate << endl;
+	currentX = x;
+	currentY = y;
+	cout << "G1 X" << currentX << " Y" << currentY << " F" << rapidInAirFeedRate << endl;
+	currentZ = zCutDepth;
+	cout << "G1 Z" << currentZ << " F" << zCutPlungeRate << endl;
+	if(liftZAtEndOfMove)
+	{
+		currentZ = safeZHeight;
+		cout << "G1 Z" << currentZ << " F" << rapidInAirFeedRate << endl;
+	}
+}
+
+// Move in air to a position
+
+void move(double x, double y)
+{
+	currentZ = safeZHeight;
+	cout << "G1 Z" << currentZ << " F" << rapidInAirFeedRate << endl;
+	currentX = x;
+	currentY = y;
+	cout << "G1 X" << currentX << " Y" << currentY << " F" << rapidInAirFeedRate << endl;
+}
+
+// Cut a straight line
+
+void cut(double x, double y)
+{
+	currentZ = zCutDepth;
+	cout << "G1 Z" << currentZ << " F" << zCutPlungeRate << endl;
+	currentX = x;
+	currentY = y;
+	cout << "G1 X" << currentX << " Y" << currentY << " F" << xyCutFeedRate << endl;
+	if(liftZAtEndOfMove)
+	{
+		currentZ = safeZHeight;
+		cout << "G1 Z" << currentZ << " F" << rapidInAirFeedRate << endl;
+	}
 }
 
 void Control()
@@ -85,7 +160,7 @@ void Control()
 			break;
 
 		case 's':
-			cout << "; Square side: ";
+			cout << "; Square side (bottom left will be the current position): ";
 			cin >> r;
 			rectangle(r, r);
 			break;
@@ -96,10 +171,30 @@ void Control()
 			centre(x, y);
 			break;
 
+		case 'm':
+			cout << "; X and Y to move to: ";
+			cin >> x >> y;
+			move(x, y);
+			break;
+
+		case 'l':
+			cout << "; X and Y to cut to: ";
+			cin >> x >> y;
+			cut(x, y);
+			break;
+
 		case 'r':
-			cout << "; Rectangle X and Y: ";
+			cout << "; Rectangle X and Y (bottom left will be the current position): ";
 			cin >> x >> y;
 			rectangle(x, y);
+			break;
+
+		case 'u':
+			liftZAtEndOfMove = !liftZAtEndOfMove;
+			cout << "; Z will ";
+			if(!liftZAtEndOfMove)
+				cout << "not ";
+			cout << "be lifted at the end of each activity." << endl;
 			break;
 
 
